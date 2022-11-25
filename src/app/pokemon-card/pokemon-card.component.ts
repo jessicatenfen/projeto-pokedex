993258c78bc458e50476b180ebe41fefc2118097
Jéssica Wazilewski <79552162+jessicatenfen@users.noreply.github.com>
2022-11-25 17:02:46 -0300

    feat : pokemon data

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Pokemon } from 'src/_model/Pokemon';
 
 @Component({
   selector: 'app-pokemon-card',
   templateUrl: './pokemon-card.component.html',
   styleUrls: ['./pokemon-card.component.sass']
 })
-export class PokemonCardComponent implements OnInit {
+export class PokemonCardComponent {
+ @Input()
+  public pokemon!: Pokemon; 
 
-  constructor() { }
+  public leadingZero(str: string | number, size: number = 3){
+    let s = String(str);
 
-  ngOnInit(): void {
+    while (s.length < (size || 2)) {
+      s = '0' + s;
+    }
+    return s;
   }
-
 }
